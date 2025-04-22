e1b992ad49f820906b944297141fbe4db377c766
niiigeruuudayooo <ds4293@columbia.edu>
2025-04-21 21:50:41 -0400

    3

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import StandardScaler
 # ---- Load and Prepare Data ----
 @st.cache_data
 def load_data():
-    df = pd.read_csv("combined_pollution_health_data.csv")
+    df = pd.read_csv("merged_health_data.csv")
     df = df.dropna()
 
     # Rename for easier reference
